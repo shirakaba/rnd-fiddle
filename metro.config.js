be2547d4266d59ac8d3428fb70318db797c794aa
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2026-03-26 22:23:34 +0900

    initial commit from expo-macos-nativescript
    https://github.com/shirakaba/expo-macos-nativescript

---
+++ metro.config.js
@@ -0,0 +1,6 @@
+const { getDefaultConfig } = require("@expo/metro-config");
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
+
+const config = makeMetroConfig(getDefaultConfig(__dirname));
+config.resolver.useWatchman = false;
+module.exports = config;
