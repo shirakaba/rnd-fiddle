7831100245e5f0c7e5d88ed8131cbdd025630b06
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2026-04-02 21:24:19 +0900

    enable webview debugging

--- App.tsx
+++ App.tsx
@@ -2,5 +2,5 @@ import { connectionProps } from "dubloon";
 import { WebView } from "react-native-webview";
 
 export default function App() {
-  return <WebView {...connectionProps({ port: 5173 })} />;
+  return <WebView {...connectionProps({ port: 5173 })} webviewDebuggingEnabled />;
 }
