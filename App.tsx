c2fe206ad7ad209063d2becd7a23dbfffbc99e64
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2026-04-04 21:54:57 +0900

    support injecting vite port as constant into React Native JS context

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { connectionProps } from "dubloon";
 import { WebView } from "react-native-webview";
 
+declare const __VITE_PORT: number;
+
 export default function App() {
+  console.log("vite port:", __VITE_PORT);
+
   return <WebView {...connectionProps({ port: 5173 })} webviewDebuggingEnabled />;
 }
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
+const { env } = require("node:process");
+
+// You may need to pass the `--clear` flag in some cases. e.g.:
+// `node --run start -- --port 8082 --clear`
 module.exports = function (api) {
   api.cache(true);
+
+  const __VITE_PORT = env.VITE_PORT ?? 5173;
+  console.log(
+    `Babel config resolved vite port of ${__VITE_PORT} (given env.VITE_PORT ${env.VITE_PORT})`,
+  );
+
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "transform-define",
+        {
+          __VITE_PORT,
+        },
+      ],
+    ],
   };
 };
