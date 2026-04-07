4ef4fb3bc1622dfee835f96e4d2f54c906f4f6c3
shirakaba <14055146+shirakaba@users.noreply.github.com>
2026-04-07 20:40:23 +0900

    turn off StrictMode for now as it's very confusing for IPC

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/select/lib/css/blueprint-select.css";
@@ -9,7 +8,8 @@ import "./styles/root.less";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  <App />,
 );
